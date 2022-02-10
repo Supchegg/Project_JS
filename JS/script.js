442da349f8b5cd88662d5af24e55f6007bacf289
Roman <roma.erichev@mail.ru>
2022-02-10 23:15:12 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+"use strict"; // Дерективна сворешенного и современного синтаксиса :D
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18", "18");
+// console.log(answer);
+
+// const answers = [];
+
+// answers [0] = prompt("Какое ваше имя?", "");
+// answers [1] = prompt("Какая ваша фамилия?", "");
+// answers [2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+const user = 'Chegg';
+alert("Привет"+ user);
+alert(`Привет ${user}`);
