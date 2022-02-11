2c80d6d8cc72dde65cf320ed0f62260822cb617b
Roman Erichev <roma.erichev@mail.ru>
2022-02-11 13:59:30 +0300

    Question one, seccuessful

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,25 @@
 
 // document.write(answers);
 
-const user = 'Chegg';
-alert("Привет"+ user);
-alert(`Привет ${user}`);
+
+
+const numberofFilms = +prompt("Сколько фильмов уже посмотрели?", "");
+
+
+const personalMovieDB = {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?",""),
+      b = prompt("На сколько оцените его?",""),
+      c = prompt("Один из последних просмотренных фильмов?",""),
+      d = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
