943aae491ef608e9e7de2199011831a129462e78
Roman <roma.erichev@mail.ru>
2022-02-11 22:03:08 +0300

    While/if/for

--- JS/script.js
+++ JS/script.js
@@ -18,26 +18,56 @@
 
 
 
-// const numberofFilms = +prompt("Сколько фильмов уже посмотрели?", "");
+const numberofFilms = prompt("Сколько фильмов уже посмотрели?", "");
 
 
-// const personalMovieDB = {
-//     count: numberofFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
-// };
+const personalMovieDB = {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-// const a = prompt("Один из последних просмотренных фильмов?",""),
-//       b = prompt("На сколько оцените его?",""),
-//       c = prompt("Один из последних просмотренных фильмов?",""),
-//       d = prompt("На сколько оцените его?","");
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?", "");
 
-// console.log(personalMovieDB);
+//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//         personalMovieDB.movies[a] = b; 
+//         console.log("Done");  
+//     } else {
+//         console.log("Error");
+//         i--;
+//     }    
+// }
+let i = 0;
+while (i < 2) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+          if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            personalMovieDB.movies[a] = b; 
+            console.log("Done");
+            i++;  
+        } else {
+            console.log("Error");
+            i--;
+        }    
+}    
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+   console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30 ) {
+   console.log("Вы киноман");
+} else { 
+    console.log("Произошла ошибка");
+}
+
+
+console.log(personalMovieDB);
 
 // const num = 50;
 
@@ -51,19 +81,12 @@
 
 // (num === 50) ? console.log("Ok") : console.log("Error");
 
-const num = 50;
 
-switch (num) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('В точку');
-        break;
-    default:
-        console.log("Не в этот раз")
-        break
-}
+
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue
+//     }
+//     console.log(i);
+// }
