7f3c7bb85db71fa7cf50b205e36b2dcf2265d5a0
Roman <roma.erichev@mail.ru>
2022-02-11 14:28:01 +0300

    Switch

--- JS/script.js
+++ JS/script.js
@@ -18,23 +18,52 @@
 
 
 
-const numberofFilms = +prompt("Сколько фильмов уже посмотрели?", "");
+// const numberofFilms = +prompt("Сколько фильмов уже посмотрели?", "");
 
 
-const personalMovieDB = {
-    count: numberofFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
+// const personalMovieDB = {
+//     count: numberofFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
 
-const a = prompt("Один из последних просмотренных фильмов?",""),
-      b = prompt("На сколько оцените его?",""),
-      c = prompt("Один из последних просмотренных фильмов?",""),
-      d = prompt("На сколько оцените его?","");
+// const a = prompt("Один из последних просмотренных фильмов?",""),
+//       b = prompt("На сколько оцените его?",""),
+//       c = prompt("Один из последних просмотренных фильмов?",""),
+//       d = prompt("На сколько оцените его?","");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log("Error");
+// } else if (num > 100) {
+//     console.log("Много");
+// } else {
+//     console.log("Ok");
+// }
+
+// (num === 50) ? console.log("Ok") : console.log("Error");
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку');
+        break;
+    default:
+        console.log("Не в этот раз")
+        break
+}
