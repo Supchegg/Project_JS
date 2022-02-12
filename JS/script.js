2275b357842051a42d432a6b2080d1e0c7c68e93
Roman <roma.erichev@mail.ru>
2022-02-12 16:48:31 +0300

    Learn callback function

--- JS/script.js
+++ JS/script.js
@@ -165,3 +165,13 @@ function writeYourGenres() {
 // const test = "12.2px";
 // console.log(parseInt(test));
 // console.log(parseFloat(test));
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log(`Я прошел этот урок`);
+}
+learnJS("JavaScript", done); // Callback функции
