1da401ed37795648dcc169e47b7fa4baa9e95254
Roman <roma.erichev@mail.ru>
2022-02-12 17:32:49 +0300

    destuction object

--- JS/script.js
+++ JS/script.js
@@ -175,3 +175,41 @@ function done() {
     console.log(`Я прошел этот урок`);
 }
 learnJS("JavaScript", done); // Callback функции
+
+// const obj = new Object();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        bg: "red"
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+options.makeTest();
+// console.log(options["colors"]["border"]);
+
+// console.log(Object.keys(options).length);
+
+const{border, bg} = options.colors; // Диструктаризация
+console.log(border);
+// console.log(options.name);
+
+// delete options.name;
+// console.log(options);
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for ( let i in options[key] ) {
+//             console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+//             counter++;   
+//         }
+//     } else {
+//         console.log(`Свойства ${key} имеет значение ${options[key]}`);  
+//         counter++;
+//     }
+// }
+// console.log(counter);
