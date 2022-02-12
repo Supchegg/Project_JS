d1bec2d5c7ff087b6b6cfd04bd71706fc3a220eb
Roman <roma.erichev@mail.ru>
2022-02-12 20:39:49 +0300

    learn first algoritm

--- JS/script.js
+++ JS/script.js
@@ -14,202 +14,233 @@
 // answers [1] = prompt("Какая ваша фамилия?", "");
 // answers [2] = prompt("Сколько вам лет?", "");
 
-// document.write(answers);
+// // document.write(answers);
 
 
 
-let numberofFilms; 
+// let numberofFilms; 
 
-function start() {
-    numberofFilms = prompt("Сколько фильмов уже посмотрели?", "");
+// function start() {
+//     numberofFilms = prompt("Сколько фильмов уже посмотрели?", "");
 
-    while(numberofFilms == "" || numberofFilms == null || isNaN(numberofFilms)){
-        numberofFilms = prompt("Сколько фильмов уже посмотрели?", "");
+//     while(numberofFilms == "" || numberofFilms == null || isNaN(numberofFilms)){
+//         numberofFilms = prompt("Сколько фильмов уже посмотрели?", "");
 
-    }
-}
+//     }
+// }
 
-start();
-
-
-const personalMovieDB = {
-    count: numberofFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
+// start();
 
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из последних просмотренных фильмов?", ""),
-              b = prompt("На сколько оцените его?", "");
+// const personalMovieDB = {
+//     count: numberofFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+
+
+// function rememberMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt("Один из последних просмотренных фильмов?", ""),
+//               b = prompt("На сколько оцените его?", "");
     
-        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-            personalMovieDB.movies[a] = b; 
-            console.log("Done");  
-        } else {
-            console.log("Error");
-            i--;
-        }    
-    }
-
-}
-
-rememberMyFilms();
-// let i = 0;
-// while (i < 2) {
-//     const a = prompt("Один из последних просмотренных фильмов?", ""),
-//           b = prompt("На сколько оцените его?", "");
-//           if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//         if (a != null && b != null && a != "" && b != "" && a.length < 50) {
 //             personalMovieDB.movies[a] = b; 
-//             console.log("Done");
-//             i++;  
+//             console.log("Done");  
 //         } else {
 //             console.log("Error");
 //             i--;
 //         }    
-// }    
-function detectPersonalLvl() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
-       console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30 ) {
-       console.log("Вы киноман");
-    } else { 
-        console.log("Произошла ошибка");
-    }
-}
-
-detectPersonalLvl();
-
-console.log(personalMovieDB);
-
-function showMyDb(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    } 
-}
-showMyDb(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.geners[i-1] = prompt(`Ваш любимый жанр под номером ${i}`,"");
-    }
-}
-// const num = 50;
-
-// if (num < 49) {
-//     console.log("Error");
-// } else if (num > 100) {
-//     console.log("Много");
-// } else {
-//     console.log("Ok");
-// }
-
-// (num === 50) ? console.log("Ok") : console.log("Error");
-
-
-
-// for (let i = 1; i < 8; i++) {
-//     if (i === 6) {
-//         // break;
-//         continue
 //     }
-//     console.log(i);
-// }
-// let num = 20;
 
-// function GetMessage(text) {
-//     console.log(text);
-//     num = 10;
 // }
 
-// GetMessage("ssd");
-// console.log(num);
-
-// function calc(a, b) {
-//     return (a + b);
-// }
-
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-
-// function ret() {
-//     let num = 50;
-//     return num;
-// }
-
-// const anotherNum = ret();
-// console.log(anotherNum);
-
-// const logger = function() {
-//     console.log("Hi");
-// };
-// logger();
-
-// const calc = (a, b) => { return a + b };
-
-// const str = "test";
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
-
-// let fruit = "Some fruit";
-// console.log(fruit.indexOf("fruit"));
-
-// const logg = "Hello world";
-// console.log(logg.slice(6, 11));
-
-// const test = "12.2px";
-// console.log(parseInt(test));
-// console.log(parseFloat(test));
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log(`Я прошел этот урок`);
-}
-learnJS("JavaScript", done); // Callback функции
-
-// const obj = new Object();
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: "black",
-        bg: "red"
-    },
-    makeTest: function() {
-        console.log("Test");
-    }
-};
-options.makeTest();
-// console.log(options["colors"]["border"]);
-
-// console.log(Object.keys(options).length);
-
-const{border, bg} = options.colors; // Диструктаризация
-console.log(border);
-// console.log(options.name);
-
-// delete options.name;
-// console.log(options);
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for ( let i in options[key] ) {
-//             console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
-//             counter++;   
-//         }
-//     } else {
-//         console.log(`Свойства ${key} имеет значение ${options[key]}`);  
-//         counter++;
+// rememberMyFilms();
+// // let i = 0;
+// // while (i < 2) {
+// //     const a = prompt("Один из последних просмотренных фильмов?", ""),
+// //           b = prompt("На сколько оцените его?", "");
+// //           if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+// //             personalMovieDB.movies[a] = b; 
+// //             console.log("Done");
+// //             i++;  
+// //         } else {
+// //             console.log("Error");
+// //             i--;
+// //         }    
+// // }    
+// function detectPersonalLvl() {
+//     if (personalMovieDB.count < 10) {
+//         console.log("Просмотрено довольно мало фильмов");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+//        console.log("Вы классический зритель");
+//     } else if (personalMovieDB.count >= 30 ) {
+//        console.log("Вы киноман");
+//     } else { 
+//         console.log("Произошла ошибка");
 //     }
 // }
-// console.log(counter);
+
+// detectPersonalLvl();
+
+// console.log(personalMovieDB);
+
+// function showMyDb(hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     } 
+// }
+// showMyDb(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for (let i = 1; i <= 3; i++) {
+//         personalMovieDB.geners[i-1] = prompt(`Ваш любимый жанр под номером ${i}`,"");
+//     }
+// }
+// // const num = 50;
+
+// // if (num < 49) {
+// //     console.log("Error");
+// // } else if (num > 100) {
+// //     console.log("Много");
+// // } else {
+// //     console.log("Ok");
+// // }
+
+// // (num === 50) ? console.log("Ok") : console.log("Error");
+
+
+
+// // for (let i = 1; i < 8; i++) {
+// //     if (i === 6) {
+// //         // break;
+// //         continue
+// //     }
+// //     console.log(i);
+// // }
+// // let num = 20;
+
+// // function GetMessage(text) {
+// //     console.log(text);
+// //     num = 10;
+// // }
+
+// // GetMessage("ssd");
+// // console.log(num);
+
+// // function calc(a, b) {
+// //     return (a + b);
+// // }
+
+// // console.log(calc(4, 3));
+// // console.log(calc(5, 6));
+
+// // function ret() {
+// //     let num = 50;
+// //     return num;
+// // }
+
+// // const anotherNum = ret();
+// // console.log(anotherNum);
+
+// // const logger = function() {
+// //     console.log("Hi");
+// // };
+// // logger();
+
+// // const calc = (a, b) => { return a + b };
+
+// // const str = "test";
+// // console.log(str.toUpperCase());
+// // console.log(str.toLowerCase());
+
+// // let fruit = "Some fruit";
+// // console.log(fruit.indexOf("fruit"));
+
+// // const logg = "Hello world";
+// // console.log(logg.slice(6, 11));
+
+// // const test = "12.2px";
+// // console.log(parseInt(test));
+// // console.log(parseFloat(test));
+
+// // function learnJS(lang, callback) {
+// //     console.log(`Я учу: ${lang}`);
+// //     callback();
+// // }
+
+// // function done() {
+// //     console.log(`Я прошел этот урок`);
+// // }
+// // learnJS("JavaScript", done); // Callback функции
+
+// // // const obj = new Object();
+// // const options = {
+// //     name: 'test',
+// //     width: 1024,
+// //     height: 1024,
+// //     colors: {
+// //         border: "black",
+// //         bg: "red"
+// //     },
+// //     makeTest: function() {
+// //         console.log("Test");
+// //     }
+// // };
+// // options.makeTest();
+// // // console.log(options["colors"]["border"]);
+
+// // // console.log(Object.keys(options).length);
+
+// // const{border, bg} = options.colors; // Диструктаризация
+// // console.log(border);
+// // console.log(options.name);
+
+// // delete options.name;
+// // console.log(options);
+// // let counter = 0;
+// // for (let key in options) {
+// //     if (typeof(options[key]) === 'object') {
+// //         for ( let i in options[key] ) {
+// //             console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+// //             counter++;   
+// //         }
+// //     } else {
+// //         console.log(`Свойства ${key} имеет значение ${options[key]}`);  
+// //         counter++;
+// //     }
+// // }
+// // console.log(counter);
+
+// // Массивы
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// // arr.sort(compareNum);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// // arr.pop();
+// // arr.push(10);
+// // console.log(arr);
+
+// // for (let i=0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
+
+// // for (let value of arr) { // Здесь уместно break и сщтегтшу
+// //     console.log(value);
+// // }
+
+// arr.forEach(function(kek, i, arr) {
+//     console.log(`${i}: ${kek} внутри массива ${arr}`); // Перебор массива с Callback функцией
+// });
+
+const str = prompt("", "");
+const products = str.split(", "); //Превращает строку в массив
+console.log(products);
+// products.sort(); // Сортирует по алфавиту
+console.log(products.join("; ")); 
